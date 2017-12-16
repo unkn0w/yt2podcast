c5bebc1e244e1738d73a155373365eca1362785c
Unknow <unknow@uw-team.org>
2017-12-16 15:14:22 +0100

    example config

--- config.php
+++ config.php
@@ -1,8 +1,10 @@
 <?php
-define('PODCAST_NAME','');
-define('PODCAST_DESC','');
-define('PODCAST_LANG','');
-define('PODCAST_AUTHOR','');
-define('PODCAST_SUMMARY','');
-define('PODCAST_CATEGORY','');
-define('PODCAST_IMAGE','');
+define('PODCAST_NAME','My Awesome Podcast');
+define('PODCAST_DESC','THis is some podcast abaout bananas!');
+define('PODCAST_LANG','PL'); // just two letters
+define('PODCAST_AUTHOR',''); // just Your name
+define('PODCAST_CATEGORY',''); // select one from here https://gist.github.com/skattyadz/814315
+define('PODCAST_IMAGE',''); // put URL to some JPG/PNG image. Minimum size is 1400x1400, max is 2400x2400. It have to be rectangle!
+
+// Youtube API key - please generate Your own on https://console.developers.google.com/apis/ (Create new project, ang obtain new API KEY)
+define('YOUTUBE_API_KEY','');
