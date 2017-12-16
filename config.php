f1e265f54dfdfe6a257f87d55fcb0a2305effae5
Unknow <unknow@uw-team.org>
2017-12-16 15:09:22 +0100

    initial commit

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+define('PODCAST_NAME','');
+define('PODCAST_DESC','');
+define('PODCAST_LANG','');
+define('PODCAST_AUTHOR','');
+define('PODCAST_SUMMARY','');
+define('PODCAST_CATEGORY','');
+define('PODCAST_IMAGE','');
