261b86a12ce8e3f9ce5ec11333f6708b309a4888
Unknow <unknow@uw-team.org>
2017-12-19 10:47:32 +0100

    fixed structure of XML and added correct timestamp to episodes

--- podcast.php
+++ podcast.php
@@ -21,18 +21,19 @@ function getMovies(){
     return json_decode($output);
 }
 
-function addItem($title,$desc,$vidID,$pubDate){
+function addItem($title,$desc,$vidID){
         global $youtube;
+        $myURL = $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
         $data = $youtube->getDownloadLinks($vidID);
         $length = preg_replace('@.+clen=([0-9]+)&.+@','\1',$data['MP4']['Medium Quality - 480x360']);
         echo '<item>
           <title>'.htmlspecialchars($title).'</title>
           <description>'.htmlspecialchars($desc).'</description>
           <link>https://youtube.com/watch?v='.$vidID.'</link>
-          <pubDate>'.date('r',strtotime($pubDate)).'</pubDate>
+          <pubDate>'.date('r',strtotime($data['meta']['publishDate'])).'</pubDate>
           <guid isPermaLink="true">https://youtube.com/watch?v='.$vidID.'</guid>
-          <enclosure url="https://uw-team.org/ytd/'.$vidID.'.mp4" length="'.$length.'" type="video/mp4" />
-          <media:content url="https://uw-team.org/ytd/'.$vidID.'.mp4" fileSize="'.$length.'" type="video/mp4" />
+          <enclosure url="'.$myURL.'/getMovie.php/'.$vidID.'.mp4" length="'.$length.'" type="video/mp4" />
+          <media:content url="'.$myURL.'/getMovie.php/'.$vidID.'.mp4" fileSize="'.$length.'" type="video/mp4" />
           <itunes:subtitle>'.htmlspecialchars($title).'</itunes:subtitle>
           <itunes:summary />
           <itunes:duration>'.$data['meta']['duration'].'</itunes:duration>
@@ -41,10 +42,36 @@ function addItem($title,$desc,$vidID,$pubDate){
 
 $movies = getMovies();
 
+header('Content-Type: application/rss+xml; charset=UTF-8');
+echo '<?xml version="1.0" encoding="utf-8"?>';
+?>
+<rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:media="http://search.yahoo.com/mrss/" version="2.0">
+  <channel>
+    <title><?php echo PODCAST_NAME; ?></title>
+    <description><?php echo PODCAST_DESC; ?></description>
+    <link><?php echo PODCAST_URL; ?></link>
+    <lastBuildDate><?php echo date('r'); ?></lastBuildDate>
+    <pubDate><?php echo date('r'); ?></pubDate>
+    <language><?php echo PODCAST_LANG; ?></language>
+    <author><?php echo PODCAST_AUTHOR; ?></author>
+    <copyright><?php echo PODCAST_AUTHOR; ?></copyright>
+    <itunes:subtitle><?php echo PODCAST_DESC; ?></itunes:subtitle>
+    <itunes:summary><?php echo PODCAST_DESC; ?></itunes:summary>
+    <itunes:explicit>no</itunes:explicit>
+    <itunes:author><?php echo PODCAST_AUTHOR; ?></itunes:author>
+    <itunes:email><?php echo PODCAST_EMAIL; ?></itunes:email>
+    <atom:link href="<?php echo $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" rel="self" type="application/rss+xml" />
+    <itunes:category text="Technology" />
+    <itunes:image href="<?php echo PODCAST_IMAGE; ?>" />
+    <media:thumbnail url="<?php echo PODCAST_IMAGE; ?>" />
+    <?php
 foreach($movies->items as $movie){
     addItem($movie->snippet->title,
         htmlspecialchars($movie->snippet->description),
         $movie->snippet->resourceId->videoId,
-        123
+        preg_replace('@^([^ ]+)T([^\.]+)@','\1 \2',$movie->snippet->publishedAt)
     );
 }
+?>
+  </channel>
+</rss>
